b1c3cbe5251ec35ab30453e0efd94b941a0e1fec
ramrod433 <djoletoamanor@gmail.com>
2022-04-05 19:12:47 +0000

    Updated mobile menu

---
+++ index.js
@@ -0,0 +1,11 @@
+const hamburger = document.querySelector('.menu-button')
+const menu = document.querySelector('.drop-menu')
+const logo = document.querySelector('.logo')
+const menuButton = document.querySelector('.menu-button')
+
+hamburger.addEventListener('click',()=> {
+    menu.classList.toggle('active');
+    logo.classList.toggle('active');
+    menuButton.classList.toggle('active');
+})
+
