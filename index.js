22c16b49e03d600a58fa0fbc2c2469ad42db61bb
ramrod433 <djoletoamanor@gmail.com>
2022-04-05 22:42:39 +0000

    Update linters

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const menu = document.querySelector('.drop-menu');
 const logo = document.querySelector('.logo');
 const menuButton = document.querySelector('.menu-button');
 
-hamburger.addEventListener('click', ()=> {
+hamburger.addEventListener('click', () => {
   menu.classList.toggle('active');
   logo.classList.toggle('active');
   menuButton.classList.toggle('active');
