cebb1628b66bc1c0628ddd2d5d2c28049425bc33
ramrod433 <djoletoamanor@gmail.com>
2022-04-15 11:44:34 +0000

    Update mobile menu

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const hamburger = document.querySelector('.menu-button');
 const menu = document.querySelector('.drop-menu');
 const menuButton = document.querySelector('.menu-button');
-const navmenu = document.querySelector('.drop-menu ul');
-
-hamburger.addEventListener('click', () => {
-  menu.classList.toggle('active');
+const navmenu = document.querySelector('.drop-menu');
+const main = document.querySelector('.main-scroll')
+menuButton.addEventListener('click', () => {
+  navmenu.classList.toggle('active');
+  main.classList.toggle('active')
   menuButton.classList.toggle('active');
 });
 
-navmenu.addEventListener('click', () => {
-  menu.classList.toggle('active');
-  menuButton.classList.toggle('active');
-});
+// menu.addEventListener('click', () => {
+//   menu.classList.toggle('active');
+//   menuButton.classList.toggle('active');
+// });
