88e46b7d41111f1759d5be8288b3f84577dd7b8e
ramrod433 <djoletoamanor@gmail.com>
2022-04-15 13:15:58 +0000

    Update css

--- index.js
+++ index.js
@@ -13,3 +13,21 @@ menuButton.addEventListener('click', () => {
 //   menu.classList.toggle('active');
 //   menuButton.classList.toggle('active');
 // });
+
+//Speakers
+
+let el = document.createElement('div')
+el.innerHTML = `
+<div class="speaker">
+  <div class="speaker-img-block">
+    <img class="speaker-img" src="${this.speakerImage}" alt="speaker image">
+  </div>
+  <div class="speaker-details">
+    <h4>${speakerName}</h4>
+    <em>${speakerGraduate}</em>
+    <div class="speaker-separation"></div>
+    <p>${speakerExperience}</p>
+  </div>
+</div>
+`
+speakersFeature.append(el)
